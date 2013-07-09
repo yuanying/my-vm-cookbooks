742609c8b3b8d82f116ed0982e2399cd70800554
yuanying <yuanying@fraction.jp>
2013-07-09 20:35:36 +0900

    古い Ruby と ruby-build の cookbook を削除。

--- cookbooks/ruby/attributes/default.rb
+++
@@ -1,7 +0,0 @@
-
-default[:ruby][:version]      = "1.9.3-p327"
-default[:ruby][:prefix_path]  = "/usr/local"
-
-default[:gems][:bundler][:version]  = "1.3.5"
-default[:gems][:rake][:version]     = "10.0.4"
-
