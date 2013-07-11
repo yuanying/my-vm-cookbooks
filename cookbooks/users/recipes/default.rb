e4bf55310f39e9f99347d9b9cfa5926fc6d874fe
yuanying <yuanying@fraction.jp>
2013-07-11 12:38:57 +0900

    zsh のサポートを追加。

--- cookbooks/users/recipes/default.rb
+++ cookbooks/users/recipes/default.rb
@@ -4,9 +4,26 @@ node[:groups].each do |g|
 end
 
 node[:users].each do |u|
+
+  home_dir = "/home/#{u[:name]}"
+
   user u[:name] do
     gid u[:gid]
     shell u[:shell]
     system u[:system]
+    home home_dir
+    supports :manage_home => true
+  end
+
+  if /zsh/ =~ u[:shell]
+    template "#{home_dir}/.zshenv" do
+      mode "0644"
+      source 'zshenv.erb'
+    end
+
+    template "#{home_dir}/.zshrc" do
+      mode "0644"
+      source 'zshrc.erb'
+    end
   end
 end
